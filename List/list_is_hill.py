8a126bde47a5199a597598254842bcf5b3b1db73
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2020-09-09 11:07:39 +0530

    Add files via upload

---
+++ List/list_is_hill.py
@@ -0,0 +1,37 @@
+# check whether list is in hill valley or not
+
+list=[]
+
+n=int(input("how many elements do you want to add in list : "))
+for i in range(0,n):
+  a=int(input("Please Enter The element :"))
+  list.append(a)
+  
+list1=list.copy()
+list2=list.copy()
+print(list)
+
+k=list.__len__()//2
+
+for i in range(1,k+1):
+		if list1[k] > list1[k-i]:
+			list1[k]=list1[k-i]
+			up=True
+		else:
+			up=False
+		if up==False :
+			break
+			
+for i in range(1,k+1):
+		if list2[k] > list2[k+i]:
+			list2[k]=list2[k+i]
+			down=True
+		else:
+			down=False
+		if down==False :
+			break
+		
+if up==down:
+	print("List is in Hill Valley ")
+else:
+	print("List is not in Hill Valley")
